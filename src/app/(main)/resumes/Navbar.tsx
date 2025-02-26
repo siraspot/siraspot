ddcea4eb139ebbd4c3548501443d0dc010ae594d
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-26 12:43:28 +0300

    Navbar, Clerk UserButton, nested layout.tsx

--- src/app/(main)/resumes/Navbar.tsx
+++ src/app/(main)/resumes/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <header className="shadow-sm">Navbar</header>
+    <header className="shadow-sm">
+        <div></div>
+    </header>
   )
 }
