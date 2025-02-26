465e592fb3e88b4188be445695b8f6282d1f1f37
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-26 12:48:33 +0300

    Navbar, Clerk UserButton, nested layout.tsx

--- src/app/(main)/resumes/Navbar.tsx
+++ src/app/(main)/resumes/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <header className="shadow-sm">
         <div className='max-w-7xl mx-auto p-3 flex justify-between items-center gap-3'>
             <Link href="/resumes" className="flex items-center gap-2">
-                <Image src={logo} />
+                <Image src={logo} alt='logo' />
             </Link>
         </div>
     </header>
