4e1b29476cd2bbc62401cb74598172dd82d64b87
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-26 10:55:40 +0300

    Clerk v6 authentication setup (customizations)

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
     <main>
-
-        <div>page</div>
+      <div>page</div>
     </main>
-  )
+  );
 }
