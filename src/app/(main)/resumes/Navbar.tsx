0dc0f58f7f709f8805d9f7e711f4c7b70cf3f4e3
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-26 12:46:14 +0300

    Navbar, Clerk UserButton, nested layout.tsx

--- src/app/(main)/resumes/Navbar.tsx
+++ src/app/(main)/resumes/Navbar.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
     <header className="shadow-sm">
         <div className='max-w-7xl mx-auto p-3 flex justify-between items-center gap-3'>
-            
+            <Link href="/resumes" className="text-2xl font-bold"></Link>
         </div>
     </header>
   )
