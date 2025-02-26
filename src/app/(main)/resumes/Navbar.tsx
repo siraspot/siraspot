86dc166253185d207ce06c03fa38d5735343729c
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-26 12:50:00 +0300

    Navbar, Clerk UserButton, nested layout.tsx

--- src/app/(main)/resumes/Navbar.tsx
+++ src/app/(main)/resumes/Navbar.tsx
@@ -1,16 +1,22 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import logo from "@/assets/logo.png"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "@/assets/logo.png";
 
 export default function Navbar() {
   return (
     <header className="shadow-sm">
-        <div className='max-w-7xl mx-auto p-3 flex justify-between items-center gap-3'>
-            <Link href="/resumes" className="flex items-center gap-2">
-                <Image src={logo} alt='logo' width={35} height={35} className='rounded-full' />
-            </Link>
-        </div>
+      <div className="max-w-7xl mx-auto p-3 flex justify-between items-center gap-3">
+        <Link href="/resumes" className="flex items-center gap-2">
+          <Image
+            src={logo}
+            alt="logo"
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
     </header>
-  )
+  );
 }
