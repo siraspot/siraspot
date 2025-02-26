583642493907f944026cdd4fc3499361e56824a1
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-26 11:12:09 +0300

    Clerk v6 authentication setup (custom sign-in/sign-up routes)

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <main className='flex h-screen items-center justify-center p-3'>SignIn</main>
+    <main className='flex h-screen items-center justify-center p-3'>
+        <SignIn />
+    </main>
   )
 }
