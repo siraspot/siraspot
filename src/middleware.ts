276d1bf41daae23a3feb3cd8fab35b87062c9976
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-26 10:47:04 +0300

    Clerk v6 authentication setup (middleware)

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, request) => {
+    if (!isPublicRoute(request)) {
+      await auth.protect();
+    }
+  });
 
 export const config = {
   matcher: [
