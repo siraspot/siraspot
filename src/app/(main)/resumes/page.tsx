f5333ef2fc00fcfe09d55fbdb39dc3d8901b587b
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 09:29:18 +0300

    Dark mode (next-themes, clerk/themes)

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
+import Link from "next/link";
 import React from "react";
 
 export const metadata: Metadata = {
@@ -9,7 +11,10 @@ export const metadata: Metadata = {
 export default function page() {
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      
+      <Button>
+        <Link href="/editor">
+        </Link>
+      </Button>
     </main>
   );
 }
