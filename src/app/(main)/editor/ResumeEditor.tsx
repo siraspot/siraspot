f89cabed216cc1f3e9b9bb92aa4906d71bd2b5db
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 09:41:55 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -2,7 +2,9 @@
 import React from "react";
 
 export default function ResumeEditor() {
-  return <div className="flex grow flex-col">
-    <header className="space-y-1.5 border-b px-3 py-5 text-center"></header>
-  </div>;
+  return (
+    <div className="flex grow flex-col">
+      <header className="space-y-1.5 border-b px-3 py-5 text-center"></header>
+    </div>
+  );
 }
