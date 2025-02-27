61bff94aa4e3c69a39c3e862d4532205e4e60a65
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 10:19:27 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/forms/GeneralInfoForm.tsx
+++ src/app/(main)/editor/forms/GeneralInfoForm.tsx
@@ -1,8 +1,11 @@
 import { GeneralInfoValues } from "@/lib/validation";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { ZodReadonly } from "zod";
 
 export default function GeneralInfoForm() {
-  const form = useForm<GeneralInfoValues>;
+  const form = useForm<GeneralInfoValues>({
+    resolver: ZodReadonly
+  })
   return <div>GeneralInfoForm</div>;
 }
