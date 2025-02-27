57e571da7e58110ece808b098ce74b3153512b8e
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 09:12:26 +0300

    Dark mode (next-themes, clerk/themes)

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import { useTheme } from "next-themes";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 export default function ThemeToggle() {
   const { setTheme } = useTheme();
