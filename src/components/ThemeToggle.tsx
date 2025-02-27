34f872f38768780c9d12b5e2b3e51ec4519f8ac4
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 09:07:38 +0300

    Dark mode (next-themes, clerk/themes)

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import React from 'react'
+import { useTheme } from 'next-themes'
 
 export default function ThemeToggle() {
+    const { theme, setTheme } = useTheme();
   return (
     <div>ThemeToggle</div>
   )
