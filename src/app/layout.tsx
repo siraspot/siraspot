1daa8b96adf94a14bab124455bc86802f6792a9b
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 09:04:29 +0300

    Dark mode (next-themes, clerk/themes)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <ThemeProvider>{children}</ThemeProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>{children}</ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
