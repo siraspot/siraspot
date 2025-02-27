39ec830bf3ac58e99ee7df41c2bad8e1dcff60e8
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 09:23:05 +0300

    Dark mode (next-themes, clerk/themes)

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "page",
+  description: "page",
+}
 
 export default function page() {
   return (
