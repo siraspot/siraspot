76c3981744fd222c5d94d6cfaf313d5f81468c03
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 09:39:17 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/page.tsx
+++ src/app/(main)/editor/page.tsx
@@ -1,14 +1,12 @@
-import { Metadata } from 'next'
-import React from 'react'
-import ResumeEditor from './ResumeEditor'
+import { Metadata } from "next";
+import React from "react";
+import ResumeEditor from "./ResumeEditor";
 
 export const metadata: Metadata = {
-    title: 'Design your resume',
-    description: 'page'
-}
+  title: "Design your resume",
+  description: "page",
+};
 
 export default function page() {
-  return (
-    <ResumeEditor />
-  )
+  return <ResumeEditor />;
 }
