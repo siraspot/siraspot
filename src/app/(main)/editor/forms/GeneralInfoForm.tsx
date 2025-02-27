09f407a59ddd8319e79d16beb33c4aa62bfe56e1
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 10:17:25 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/forms/GeneralInfoForm.tsx
+++ src/app/(main)/editor/forms/GeneralInfoForm.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 export default function GeneralInfoForm() {
-  const form = useForm
+  const form = useForm()
     return (
     <div>GeneralInfoForm</div>
   )
