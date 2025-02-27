e194c3cb241b5f41ce424b2e5c0336356d19e4f2
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-02-27 10:04:03 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -15,7 +15,9 @@ export default function ResumeEditor() {
       </header>
       <main className="relative grow">
         <div className="absolute bottom-0 top-0 flex w-full">
-            <div className="w-full md:w-1/2"></div>
+            <div className="w-full md:w-1/2">
+            left
+            </div>
         </div>
       </main>
       <footer className="w-full border-t px-3 py-5 text-center">
