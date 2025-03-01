d6b7416986a09941d1d2c7a20824dc16235b4d83
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-01 20:45:12 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/forms/EducationForm.tsx
+++ src/app/(main)/editor/forms/EducationForm.tsx
@@ -1,6 +1,10 @@
+import { EditorFormProps } from '@/lib/types'
 import React from 'react'
 
-export default function EducationForm() {
+export default function EducationForm({
+    resumeData,
+    setResumeData,
+  }: EditorFormProps) {
   return (
     <div>EducationForm</div>
   )
