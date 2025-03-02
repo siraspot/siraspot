539ba6955c17547c16120dd59f6a9e9dfa337b44
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-02 14:28:45 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/forms/SummaryForm.tsx
+++ src/app/(main)/editor/forms/SummaryForm.tsx
@@ -1,4 +1,17 @@
 import React from 'react'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form";
+  import { Textarea } from "@/components/ui/textarea";
+  import { EditorFormProps } from "@/lib/types";
+  import { summarySchema, SummaryValues } from "@/lib/validation";
+  import { zodResolver } from "@hookform/resolvers/zod";
+  import { useForm } from "react-hook-form";
 
 export default function SummaryForm() {
   return (
