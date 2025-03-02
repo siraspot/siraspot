5659ab3392e856dce7bee39ba011b1e835d803e4
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-02 14:55:14 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -16,5 +16,30 @@ export default function ResumePreviewSection({
   setResumeData,
   className,
 }: ResumePreviewSectionProps) {
-  return <div>ResumePreviewSection</div>;
+  return (
+    <div
+      className={cn("group relative hidden w-full md:flex md:w-1/2", className)}
+    >
+      <div className="absolute left-1 top-1 flex flex-none flex-col gap-3 opacity-50 transition-opacity group-hover:opacity-100 lg:left-3 lg:top-3 xl:opacity-100">
+        <ColorPicker
+          color={resumeData.colorHex}
+          onChange={(color) =>
+            setResumeData({ ...resumeData, colorHex: color.hex })
+          }
+        />
+        <BorderStyleButton
+          borderStyle={resumeData.borderStyle}
+          onChange={(borderStyle) =>
+            setResumeData({ ...resumeData, borderStyle })
+          }
+        />
+      </div>
+      <div className="flex w-full justify-center overflow-y-auto bg-secondary p-3">
+        <ResumePreview
+          resumeData={resumeData}
+          className="max-w-2xl shadow-md"
+        />
+      </div>
+    </div>
+  );
 }
