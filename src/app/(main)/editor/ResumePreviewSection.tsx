c06bafeb282b6dc20eb693d82db5bead0b0028df
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-02 14:53:49 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ResumePreview from "@/components/ResumePreview";
+import { cn } from "@/lib/utils";
+import { ResumeValues } from "@/lib/validation";
+import BorderStyleButton from "./BorderStyleButton";
+import ColorPicker from "./ColorPicker";
 
 export default function ResumePreviewSection() {
   return <div>ResumePreviewSection</div>;
