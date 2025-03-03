ceb60ad868f8808c575a2a65612a815573eec7b6
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-03 15:28:29 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/useAutoSaveResume.tsx
+++ src/app/(main)/editor/useAutoSaveResume.tsx
@@ -5,6 +5,11 @@ import React from 'react'
 export default function useAutoSaveResume(resume: ResumeValues) {
 
     const debouncedResumeData = useDebounce(resumeData, 1500);
+
+    const [lastSavedData, setLastSavedData] = React.useState(
+        structuredClone(resumeData),
+      );
+
   return (
     <div>useAutoSaveResume</div>
   )
