f1ed611dc4c147f4c8ab9020ea57761b64d3063f
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-04 02:44:10 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/actions.ts
+++ src/app/(main)/editor/actions.ts
@@ -8,4 +8,7 @@ export async function saveResume(values: ResumeValues) {
     const { id } = values;
 
     console.log("received values", values);
+
+    const { photo, workExperiences, educations, ...resumeValues } =
+    resumeSchema.parse(values);
 }
