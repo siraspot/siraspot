49c217c8eadcd67eb9fe1e94f5fa960659263179
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-03 14:06:13 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/forms/WorkExperienceForm.tsx
+++ src/app/(main)/editor/forms/WorkExperienceForm.tsx
@@ -94,7 +94,7 @@ export default function WorkExperienceForm({
       </div>
       <Form {...form}>
         <form className="space-y-3">
-          <DndContext></DndContext>
+          <DndContext sensors={sensors}></DndContext>
           {fields.map((field, index) => (
             <WorkExperienceItem
               id={field.id}
