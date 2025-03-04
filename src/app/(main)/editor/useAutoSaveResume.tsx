a71a9724c95b757579e8813a9be9b54e3389f989
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-04 10:18:26 +0300

    Resume editor (React Hook Form, FormField, useFieldArray, Zod refine, server actions, auto-save hook)

--- src/app/(main)/editor/useAutoSaveResume.tsx
+++ src/app/(main)/editor/useAutoSaveResume.tsx
@@ -22,7 +22,7 @@ export default function useAutoSaveResume(resumeData: ResumeValues) {
     async function save() {
       try {
         setIsSaving(true);
-        //     setIsError(false);
+            setIsError(false);
         //     const newData = structuredClone(debouncedResumeData);
         //     const updatedResume = await saveResume({
         //       ...newData,
