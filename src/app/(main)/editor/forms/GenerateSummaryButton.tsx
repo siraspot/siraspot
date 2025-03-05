3b2d560ec7b8713d0dc95db32872ea2b3a7c355b
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-06 02:44:17 +0300

    AI features (ChatGPT API)

---
+++ src/app/(main)/editor/forms/GenerateSummaryButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface GenerateSummaryButtonProps {
+    resumeData: ResumeValues;
+    onSummaryGenerated: (summary: string) => void;
+  }
+export default function GenerateSummaryButton() {
+  return (
+    <div>GenerateSummaryButton</div>
+  )
+}
