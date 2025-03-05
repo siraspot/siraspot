ac75da8961752a84ec885f1b768ddadd0f4183a6
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-06 02:28:48 +0300

    AI features (ChatGPT API)

--- src/app/(main)/editor/forms/actions.ts
+++ src/app/(main)/editor/forms/actions.ts
@@ -1,3 +1,10 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import {
+  GenerateSummaryInput,
+  generateSummarySchema,
+  GenerateWorkExperienceInput,
+  generateWorkExperienceSchema,
+  WorkExperience,
+} from "@/lib/validation";
