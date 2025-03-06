00dc6d8a38b721e9255e8303711bae0f9ed0dcc4
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-06 11:01:18 +0300

    AI features (ChatGPT API)

--- src/app/(main)/editor/forms/GenerateWorkExperienceButton.tsx
+++ src/app/(main)/editor/forms/GenerateWorkExperienceButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
 
 export default function GenerateWorkExperienceButton() {
   return (
