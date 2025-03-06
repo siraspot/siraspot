2cb2adc453040fffeb634133f436f35cf47b7520
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-06 16:15:14 +0300

    --- 7:40

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,23 +1,61 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { resumeDataInclude } from "@/lib/types";
+import { auth } from "@clerk/nextjs/server";
 import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
+import ResumeItem from "./ResumeItem";
 
 export const metadata: Metadata = {
   title: "Your resumes",
   description: "page",
 }
 
-export default function page() {
+export default async function page() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const [resumes, totalCount] = await Promise.all([
+    prisma.resume.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      include: resumeDataInclude,
+    }),
+    prisma.resume.count({
+      where: {
+        userId,
+      },
+    }),
+  ]);
+
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild>
+      <Button asChild className="mx-auto flex w-fit gap-2">
         <Link href="/editor">
           <PlusSquare className="size-5" />
-          <span>New Resume</span>
+          New resume
         </Link>
       </Button>
+      <div className="space-y-1">
+        <h1 className="text-3xl font-bold">Your resumes</h1>
+        <p>Total: {totalCount}</p>
+      </div>
+      <div className="flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {resumes.map((resume) => (
+          <>
+            <ResumeItem key={resume.id} resume={resume} />
+          </>
+        ))}
+      </div>
     </main>
   );
 }
