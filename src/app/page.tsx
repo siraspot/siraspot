f8810c7c9beaf818a2fdda0fbd60603d7a7d59bf
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-08 22:19:13 +0300

    Responsive landing page (Shadcn custom button variant)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
-import { Button } from "@/components/ui/button";
 import resumePreview from "@/assets/resume-preview1.svg";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
