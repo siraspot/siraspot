5e88d6993319986d15f20ca4cbb220fe5b64a87f
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-09 22:29:59 +0300

    Global modal/dialog with Zustand

--- src/components/premium/PremiumModal.tsx
+++ src/components/premium/PremiumModal.tsx
@@ -27,6 +27,7 @@ export default function PremiumModal() {
                   </li>
                 ))}
               </ul>
+              <Button>Get Premium</Button>
             </div>
           </div>
         </div>
