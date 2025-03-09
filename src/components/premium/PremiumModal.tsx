3f9fee4aa04a84593f51594b6b648d880a0d85f2
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-09 15:04:05 +0300

    Global modal/dialog with Zustand

--- src/components/premium/PremiumModal.tsx
+++ src/components/premium/PremiumModal.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import React from 'react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog';
+import React from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 
 export default function PremiumModal() {
   return (
     <Dialog open>
-        <DialogContent className="max-w-2xl">
+      <DialogContent className="max-w-2xl">
         <DialogHeader>
-        <DialogTitle>Resume Builder AI Premium</DialogTitle></DialogHeader>
-        </DialogContent>
+          <DialogTitle>Resume Builder AI Premium</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
     </Dialog>
-  )
+  );
 }
