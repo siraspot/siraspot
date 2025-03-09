d32221e63842de3722e380669229bdea6a911c3f
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-09 22:54:36 +0300

    Global modal/dialog with Zustand

--- src/app/(main)/resumes/CreateResumeButton.tsx
+++ src/app/(main)/resumes/CreateResumeButton.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import usePremiumModal from "@/hooks/usePremiumModal";
+import { PlusSquare } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 interface CreateResumeButtonProps {
