9b1c4bfcf53a4dbda958eb20c265e8a3453508ec
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-09 15:03:22 +0300

    Global modal/dialog with Zustand

--- src/components/premium/PremiumModal.tsx
+++ src/components/premium/PremiumModal.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React from 'react'
-import { Dialog, DialogContent } from '../ui/dialog';
+import { Dialog, DialogContent, DialogHeader } from '../ui/dialog';
 
 export default function PremiumModal() {
   return (
     <Dialog open>
-        <DialogContent className="max-w-2xl"></DialogContent>
+        <DialogContent className="max-w-2xl">
+        <DialogHeader></DialogHeader>
+        </DialogContent>
     </Dialog>
   )
 }
