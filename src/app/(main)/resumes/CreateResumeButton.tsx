ca35923ced08590ccef92de69e7bfd8971147503
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-09 22:53:46 +0300

    Global modal/dialog with Zustand

--- src/app/(main)/resumes/CreateResumeButton.tsx
+++ src/app/(main)/resumes/CreateResumeButton.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+interface CreateResumeButtonProps {
+    canCreate: boolean;
+  }
+
 export default function CreateResumeButton() {
   return (
     <div>CreateResumeButton</div>
