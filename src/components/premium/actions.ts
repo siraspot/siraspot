059604b3ff9eb11f9ec850d0183799ed8d7a2196
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-11 13:37:41 +0300

    Stripe Checkout (subscriptions)

--- src/components/premium/actions.ts
+++ src/components/premium/actions.ts
@@ -4,8 +4,8 @@ import stripe from "@/lib/stripe";
 import { currentUser } from "@clerk/nextjs/server";
 
 export async function createCheckoutSession(priceId: string) {
-    const user = await currentUser();
-    if (!user) {
-        throw new Error("Unauthorized");
-      }
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
 }
