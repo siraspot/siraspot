dc6fc84a68aff840cb4153d3c478a99c75722a17
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-14 20:05:29 +0300

    Type-safe environment variables with t3-env

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,8 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-    server: {
-        POSTGRES_URL: z.string().min(1),
+  server: {
+    POSTGRES_URL: z.string().min(1),
     POSTGRES_PRISMA_URL: z.string().min(1),
     POSTGRES_URL_NO_SSL: z.string().min(1),
     POSTGRES_URL_NON_POOLING: z.string().min(1),
@@ -15,7 +15,7 @@ export const env = createEnv({
     BLOB_READ_WRITE_TOKEN: z.string().min(1),
     OPENAI_API_KEY: z.string().min(1),
     STRIPE_SECRET_KEY: z.string().min(1),
-    },
+  },
   schema: z.object({
     NODE_ENV: z.string().default("development"),
     PORT: z.string().default("3000"),
