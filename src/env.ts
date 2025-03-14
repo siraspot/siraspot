b46400df11f1eaa359fd028f6de5a17a40db8745
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-14 20:04:44 +0300

    Type-safe environment variables with t3-env

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
+    server: {}
   schema: z.object({
     NODE_ENV: z.string().default("development"),
     PORT: z.string().default("3000"),
