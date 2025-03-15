0e9cd0e44e68d1400db56cde6f5ad7b956407a5c
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-16 00:46:41 +0300

    Save/fetch subscriptions (React cache, context provider)

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 import React from "react";
 import ResumeItem from "./ResumeItem";
-import CreateResumeButton from "./CreateResumeButton";
+// import CreateResumeButton from "./CreateResumeButton";
 
 export const metadata: Metadata = {
   title: "Your resumes",
@@ -37,7 +37,7 @@ export default async function page() {
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <CreateResumeButton canCreate={totalCount < 3} />
+      {/* <CreateResumeButton canCreate={totalCount < 3} /> */}
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your resumes</h1>
         <p>Total: {totalCount}</p>
