cb9acdede4cacd597940b610c01f182548226c78
abdulApp <abdulrahman_alamoudi@hotmail.com>
2025-03-16 01:21:51 +0300

    Save/fetch subscriptions (React cache, context provider)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,9 @@
 import PremiumModal from "@/components/premium/PremiumModal";
 import Navbar from "./Navbar";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const {userId} = await auth();
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
